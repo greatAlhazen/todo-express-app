db42f52d53c9252fdaffda4662ca8e93108a2ff7
Enes Uraz <axeprime91@gmail.com>
2023-01-19 10:41:33 +0300

    add validation with express-validator

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,7 +1,14 @@
 import Todos from '../models/todos.js';
 import User from '../models/todos.js';
+import { validationResult } from 'express-validator';
 
 export const createTodo = async(req,res,next) =>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+            const error = errors.array()[0].msg
+            req.flash('error',error);
+            res.redirect('/');
+    }else{
         try{
             const todo = new Todos(req.body);
             todo.owner = req.user._id;
@@ -11,6 +18,8 @@ export const createTodo = async(req,res,next) =>{
         }catch(err){
             next(err);
         }
+    }
+        
 };
 
 export const fetchTodos = async(req,res,next) =>{
@@ -44,16 +53,24 @@ export const updateTodosPage = async(req,res,next) =>{
 }
 
 export const updateTodos = async(req,res,next) =>{
-    try{
-        req.body.owner = req.user._id;
-        await Todos.findByIdAndUpdate(req.params.id,{
-            '$set':req.body
-        },{new:true});
-        req.flash('success','successfully update todo');
-        res.redirect('/');
-    }catch(err){
-        next(err);
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        const error = errors.array()[0].msg
+        req.flash('error',error);
+        res.redirect(`/${req.params.id}`);
+    }else{
+        try{
+            req.body.owner = req.user._id;
+            await Todos.findByIdAndUpdate(req.params.id,{
+                '$set':req.body
+            },{new:true});
+            req.flash('success','successfully update todo');
+            res.redirect('/');
+        }catch(err){
+            next(err);
+        }
     }
+    
 }
 
 export const deleteTodo = async(req,res,next) =>{
--- routes/home.js
+++ routes/home.js
@@ -1,14 +1,40 @@
 import express from 'express';
 import { createTodo, deleteTodo, fetchTodos, updateTodos, updateTodosPage } from '../controllers/todos.js';
 import { authorization } from '../utils/middleware.js';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
 router.get('/',fetchTodos);
 
-router.post('/create',authorization,createTodo);
+router.post('/create',
+[body('title')
+.isString()
+.isLength({ min:3,max:25 })
+.trim()
+.withMessage('Please enter title between 1-20 characters')
+,body('desc')
+.isLength({min:5,max:420})
+.trim()
+.withMessage('Please enter description between 5 and 420 characters')
+,body('schedule').isDate().withMessage('please enter valid date')],
+authorization,
+createTodo
+);
 
-router.route('/:id').get(authorization,updateTodosPage).put(authorization,updateTodos);
+router.route('/:id').get(authorization,updateTodosPage).put(
+    [body('title')
+    .isString()
+    .isLength({ min:3,max:25 })
+    .trim()
+    .withMessage('Please enter title between 1-20 characters')
+    ,body('desc')
+    .isLength({min:5,max:420})
+    .trim()
+    .withMessage('Please enter description between 5 and 420 characters')
+    ,body('schedule').isDate().withMessage('please enter valid date')],
+authorization,
+updateTodos);
 
 router.delete('/:id',deleteTodo)
 
